17cb6136c6a9097c50a75c979a59c765ae4c01fe
MariemeBenz <marieme.benzha@gmail.com>
2025-09-11 22:34:36 +0200

    add read me

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ import { Car } from "./Car";
 import { Course } from "./Course";
 
 // Création des voitures
-const car1 = new Car("Model S", "Tesla", "Rouge", 0, 2022, false, 250);
-const car2 = new Car("Mustang", "Ford", "Bleu", 0, 2020, false, 220);
-const car3 = new Car("Civic", "Honda", "Noir", 0, 2019, false, 200);
+const car1 = new Car("Model S", "Tesla", "Rouge", 0, 2022, false, 220);
+const car2 = new Car("Classe A", "Mercedes", "Bleu", 0, 2020, false, 260);
+const car3 = new Car("Panamera", "Porsche", "Noir", 0, 2019, false, 270);
 
 // Création de la course (distance 100 km)
 const race = new Course(100, car1, car2, car3);
